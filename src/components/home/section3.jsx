8da22ec486ee44cbcfdf100d04a1bcbf7e89899f
lawalmz <lawalmlawal25@gmail.com>
2023-10-30 16:21:52 +0200

    still on the home page

--- src/components/home/section3.jsx
+++ src/components/home/section3.jsx
@@ -2,11 +2,19 @@ import React from "react";
 
 
 function Section({ darkMode }) {
+
+  const imageUrl = darkMode
+  ? "../../images/dark_logo.jpg" // Image URL for dark mode
+  : "../../images/logo2.jpg"; // Image URL for normal mode
   return (
     <div className={`part ${darkMode ? "dark" : ""}`}>
       <div className={`Pcontainer ${darkMode ? "dark" : ""}`}>
-        <div className={`section3-img ${darkMode ? "dark" : ""}`}></div>
-        <div class="vertical-line"></div>
+      <img
+          src={imageUrl}
+          alt="Luxury Lodge"
+          className="section3-img"
+        />
+        <div class= {`vertical-line ${darkMode ? "dark" : ""}`}></div>
         <div class="text">
           <h1>LuxuryLodges: Timeless Luxury, Unforgettable Adventures!</h1>
           <p>Explore our world of exceptional hospitality and discover your dream escape today. Indulge in opulence, immerse in comfort, and create memories that last a lifetime. Experience our world-class hospitality and embark on a journey of discovery and relaxation. Book your extraordinary getaway with LuxuryLodges today and let us redefine your idea of luxury.</p>
